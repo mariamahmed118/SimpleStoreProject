01ced320f007edb89ff034025388a178e826a57f
Mona Mohamed <111650146+MonaMohamedAbdelaziz2003@users.noreply.github.com>
2024-12-12 21:27:11 +0200

    Add new file or changes

--- UI.fs
+++ UI.fs
@@ -1,1 +1,14 @@
 namespace SimpleStore
+
+module UI =
+    open System
+    open System.Drawing
+    open System.Windows.Forms
+    open Logic
+    open Data
+    open Models
+
+    // Authentication Colors
+    let primaryColor = Color.FromArgb(0x14, 0x3a, 0x51)
+    let accentColor = Color.FromArgb(0xff, 0x5d, 0x68)
+    let backgroundColor = primaryColor
